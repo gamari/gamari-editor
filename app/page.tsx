bc93e7fb2bed6f021497f4cd3b45c85e7616255f
Gamari <71511096+gamari@users.noreply.github.com>
2023-11-29 14:50:15 +0000

    init project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+   <main>
+    <div>Test</div>
+
+    <Button>Test</Button>
+   </main>
+  )
+}
